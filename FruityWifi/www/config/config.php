17dd30b791c90b552aa06a5163421e7cc284408c
xtr4nge <xtr4nge@gmail.com>
2013-10-19 11:19:42 +0100

    minor fix

--- FruityWifi/www/config/config.php
+++ FruityWifi/www/config/config.php
@@ -5,10 +5,10 @@ $iface_internet="eth0";
 $iface_wifi="-";
 $iface_wifi_extra="-";
 $iface_supplicant="-";
-$supplicant_ssid="RaspberryWifi";
-$supplicant_psk="RaspberryWifi";
+$supplicant_ssid="FruityWifi";
+$supplicant_psk="FruityWifi";
 $hostapd_ssid="Wifi-R";
 $hostapd_secure="0";
-$hostapd_wpa_passphrase="RaspberryWifi";
+$hostapd_wpa_passphrase="FruityWifi";
 $url_rewrite_program="pasarela_xss.js";
 ?>
