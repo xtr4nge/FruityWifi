764ba0f37e516dc36fe96edccc7f50608c65191a
xtr4nge <xtr4nge@gmail.com>
2013-11-01 21:52:21 +0000

    Update config.php

--- FruityWifi/www/config/config.php
+++ FruityWifi/www/config/config.php
@@ -1,5 +1,5 @@
 <?
-$version="v1.2";
+$version="v1.3";
 $regex=1; // 1 (on) | 0 (off) >> web interface input validation.
 $regex_extra=" _-.[]*"; // extra characters allowed (input validation).
 $iface_internet="eth0";
