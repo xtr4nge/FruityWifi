3bfce47907ad47a222100554395383e9ee263c2a
xtr4nge <xtr4nge@gmail.com>
2014-03-01 13:56:38 +0000

    minor fix (design has been changed)

--- FruityWifi/www/page_modules.php
+++ FruityWifi/www/page_modules.php
@@ -1,6 +1,6 @@
 <? 
 /*
-	Copyright (C) 2013  xtr4nge [_AT_] gmail.com
+	Copyright (C) 2013-2014  xtr4nge [_AT_] gmail.com
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
